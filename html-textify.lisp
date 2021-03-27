fc4de43ef35b15b4023fb0091a9e8b622a5b5e41
Michael Raskin <38a938c2@rambler.ru>
2021-03-27 02:49:08 +0100

    Syncing from Monotone at 20210327-024908. Last commit message was Handle blockquotes

--- html-textify.lisp
+++ html-textify.lisp
@@ -255,6 +255,11 @@
    (type (eql :element)) (tag (eql :li)) element)
   (format nil "{>>> ~a <<<}" (call-next-method)))
 
+(defmethod html-element-to-text-dispatch
+  ((protocol show-formatting-mixin)
+   (type (eql :element)) (tag (eql :blockquote)) element)
+  (format nil ">[ ~a ]" (call-next-method)))
+
 (defclass recur-on-noscript-mixin (html-textifier-protocol) ())
 
 (defmethod html-element-to-text-dispatch
